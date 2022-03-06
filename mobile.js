bb38833476d97313373416d898c3f1a1fa796619
Bhi2710 <92436824+Bhi2710@users.noreply.github.com>
2022-03-06 18:31:52 +0530

    Add files via upload

---
+++ mobile.js
@@ -0,0 +1,24 @@
+function lenovoA7(){
+    var output_lenovo = document.getElementById("lenovoA7");
+    output_lenovo.innerText = "Samsumg A7";
+}
+
+function lenovoA(){
+    var output_lenovo = document.getElementById("lenovoA");
+    output_lenovo.innerText = "Samsumg M12";
+}
+function lenovo7(){
+    var output_lenovo = document.getElementById("lenovo7");
+    output_lenovo.innerText = "Samsumg S20";
+}
+function len(){
+    var output_lenovo = document.getElementById("len");
+    output_lenovo.innerText = "SamsumgM52";
+}
+function leno(){
+    var output_lenovo = document.getElementById("leno");
+    output_lenovo.innerText = "Samsumg M32";
+}
+
+
+    
